aa8996371a67f0957ac7892cc0af32427a820a3f
unknown <daryantotanawi1098@gmail.com>
2025-06-30 05:56:16 +0700

    Sound effect flash camera wak

--- resources/views/design.blade.php
+++ resources/views/design.blade.php
@@ -150,6 +150,8 @@
         const musicPhoto = document.getElementById('music-photo');
         const musicToggleButton = document.getElementById('music-toggle-btn');
         
+        const sfxShutter = document.getElementById('sfx-shutter');
+
         let isTransitioning = false;
         let isMuted = true;
         let currentMusic = musicDesign;
@@ -158,6 +160,8 @@
         // Inisialisasi volume
         musicDesign.volume = 0;
         musicPhoto.volume = 0;
+
+        sfxShutter.volume = 0.5;
 
         const speakerOnIcon = `<svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" /></svg>`;
         const speakerOffIcon = `<svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" /></svg>`;
@@ -221,6 +225,9 @@
             cameraContainer.classList.remove('hidden');
             cameraContainer.classList.add('animate-camera-slide');
             setTimeout(() => {
+                sfxShutter.currentTime = 0; // Ulangi suara dari awal
+                sfxShutter.play();
+                
                 cameraContainer.classList.add('animate-shutter-click');
                 fromPage.classList.add('hidden');
                 fromPage.classList.remove('flex');
@@ -263,6 +270,9 @@
     </audio>
     <audio id="music-photo" loop>
         <source src="{{ asset('music/photo-music.mp3') }}" type="audio/mpeg">
+    </audio>
+    <audio id="sfx-shutter">
+        <source src="{{ asset('sfx/camera-shutter.mp3') }}" type="audio/mpeg">
     </audio>
 
     <button id="music-toggle-btn" class="fixed bottom-6 right-6 z-50 text-white bg-black/20 p-2 rounded-full hover:bg-black/40 transition-all duration-300">
